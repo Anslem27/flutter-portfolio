6c1f733f0de77e9240894a298b03b4cab2934d9d
Anslem27 <anslembarn@gmail.com>
2022-10-25 13:13:56 +0300

    add credentials back

--- lib/services/spotify_service.dart
+++ lib/services/spotify_service.dart
@@ -6,8 +6,9 @@ import 'package:spotify/spotify.dart';
 //TODO: Fetch recently played from personal account, rather than setting up const variables
 class SpotifyService {
   //TODO: Add client secret and id here
-
-  static var credentials = SpotifyApiCredentials('CLIENT_ID', 'CLIENT_SECRET');
+  static var credentials = SpotifyApiCredentials(
+      '6ac6511f2ed1432e9917624fc7a67bef', 'd6331f83644e4d93b27f63bd97da3f3c');
+  //static var credentials = SpotifyApiCredentials('CLIENT_ID', 'CLIENT_SECRET');
   final spotify = SpotifyApi(credentials);
 
   //playlist id from which you want to fetch fav songs preferably a daily updated playlist.
